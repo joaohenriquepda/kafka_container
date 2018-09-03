1360a1875428e4fb8fb48c2c76cb95366c0337de
João Henrique <joaohenrique.p.almeida@gmail.com>
2018-09-03 20:26:21 -0300

    Adding script for execute comands

---
+++ consumer_topics.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# # bin/zookeeper-server-start.sh config/zookeeper.properties
+# echo ${HOME}${ZK_HOME}
+# # echo
+
+# echo " ----------------- "
+# echo "Criando o topic"
+# echo " ----------------- "
+# ./kafka_2.11-1.1.0/bin/kafka-server-start.sh kafka_2.11-1.1.0/config/server.properties &
+
+
+echo " ----------------- "
+echo "Consumindo o topico" $var $1
+echo " ----------------- "
+
+# echo $KAFKA_HOME
+# echo $ZOOKEEPER_PORT
+# echo ${ZOOKEEPER_PORT}
+# echo $ZK_HOME
+
+
+# ./kafka_2.11-1.1.0/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER_PORT --replication-factor 1 --partitions 1 --topic $1
+
+./$KAFKA_HOME/bin/kafka-topics.sh --list --zookeeper $ZOOKEEPER_PORT
+./kafka_2.11-1.1.0/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic $1
+
+# python3 producer.twitter.py
---
+++ list_topics.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# bin/zookeeper-server-start.sh config/zookeeper.properties
+# echo ${HOME}${ZK_HOME}
+# # echo
+#
+# echo " ----------------- "
+# echo "Startando Kafka"
+# echo " ----------------- "
+# # ./kafka_2.11-1.1.0/bin/kafka-server-start.sh kafka_2.11-1.1.0/config/server.properties
+# echo  ${ZK_HOME} "-----------" ${KAFKA_ZOOKEEPER_CONNECT}
+
+echo " ----------------- "
+echo "Listando os tópicos"
+echo " ----------------- "
+
+# echo $KAFKA_HOME
+# echo $ZOOKEEPER_PORT
+# echo ${ZOOKEEPER_PORT}
+# echo $ZK_HOME
+# ./kafka_2.11-1.1.0/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER_PORT --replication-factor 1 --partitions 1 --topic streamTwitter &
+./$KAFKA_HOME/bin/kafka-topics.sh --list --zookeeper $ZOOKEEPER_PORT
+# ./kafka_2.11-1.1.0/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic streamTwitter
+
+# python3 producer.twitter.py
